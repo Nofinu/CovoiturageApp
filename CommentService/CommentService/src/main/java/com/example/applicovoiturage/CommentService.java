d3b21e908424364dbff10d039c1434ed953dee5b
Leo lhomme <leolhommeo@gmail.com>
2023-09-06 14:42:53 +0200

    rename CommentService

--- CommentService/CommentService/src/main/java/com/example/applicovoiturage/CommentService.java
+++ CommentService/CommentService/src/main/java/com/example/applicovoiturage/CommentService.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApplicovoiturageApplication {
+public class CommentService {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApplicovoiturageApplication.class, args);
+		SpringApplication.run(CommentService.class, args);
 	}
 
 }
